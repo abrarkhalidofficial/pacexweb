ccef2f14bcc00d436567ed53ece0568b6de74938
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-02-25 10:27:55 +0500

    contact section started

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Contact from "../components/Contact";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Contact />
+    </>
+  );
 }
